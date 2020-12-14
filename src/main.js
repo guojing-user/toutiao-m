1e960697536ad5089618ec6c946cfa1c754bb0ec
guojing-user <1442404503@qq.com>
2020-12-14 11:18:15 +0800

    项目初始化-引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
+
+// 加载css全局样式
+import './styles/index.less'
+
+// 注册使用Vant 组件库
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 new Vue({
   router,
